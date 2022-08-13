3d7e51acdd7fb5be6a1cb5a7864cba81886b6c15
Luiz Loja <luizloja@gmail.com>
2022-08-12 23:56:04 -0300

    5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const name = core.getInput('path');
+    const name = core.getInput('file');
     console.log(name)
 } catch (error) {
     
