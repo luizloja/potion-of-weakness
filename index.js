2edf8c382cde20d4a7bbfb79e618d44fbf5ee767
Luiz Loja <luizloja@gmail.com>
2022-08-18 14:50:32 -0300

    test 10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const gitGosta = require('@actions/github');
 
+
 const ElixirAnalyzer = require('./app/ElixirAnalyzer.js')
 
 
@@ -8,7 +9,8 @@ try {
     // const path = core.getInput('coverageFile');
     const path = "test_coverage.txt";
     const files = ["lib/schema/schema.ex"]
-    const client = new gitGosta.GitHub(core.getInput('token', { required: false }))
+
+    const client = new gitGosta.getOctokit(core.getInput('token', { required: false }))
     const minimalTestCoverage = 70
     let elixirAnalyzer = new ElixirAnalyzer(path, minimalTestCoverage)
     elixirAnalyzer.execute()
